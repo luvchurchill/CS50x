68b9764c8257f19ebeba6650c7507347754d968b
luvchurchill <46406654+luvchurchill@users.noreply.github.com>
2023-11-05 07:59:49 -0500

    Add files via upload

---
+++ helpers for smiley.c
@@ -0,0 +1,15 @@
+#include "helpers.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtBlue == 0)
+            {
+                image[i][j].rgbtBlue = 101;
+            }
+        }
+    }
+}
